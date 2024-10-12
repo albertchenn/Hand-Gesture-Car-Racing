8c2d5d5a665d8827870e07b6472559540676be77
Dakota Hanks <dakotahanks1020@gmail.com>
2024-10-12 18:24:05 -0400

    add timer functionality

---
+++ public/src/timer.js
@@ -0,0 +1,50 @@
+const timer = document.getElementsByClassName("timer")[0];
+const stopwatch  = document.getElementsByClassName("stopwatch")[0];
+
+let elapsedTime = 0;
+let isOn = false;
+let intervalID;
+
+function formatTime(elapsedTime) {
+    const min_conversion = 60000; 
+    const sec_conversion = 1000;
+
+    let milliseconds = elapsedTime;
+
+    let minutes = Math.floor(elapsedTime / min_conversion);
+    let seconds = Math.floor((elapsedTime % min_conversion) / sec_conversion);
+    milliseconds = elapsedTime % sec_conversion;
+
+    minutes = String(minutes).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0');
+    milliseconds = String(milliseconds).padStart(3, '0');
+
+    return `${minutes}:${seconds}:${milliseconds}`;
+}
+
+function startTimer() {
+    const updateDelay = 10
+    let startTime = Date.now();
+
+   intervalID =  setInterval( () => {
+        elapsedTime = Date.now() - startTime;
+
+        stopwatch.innerHTML = formatTime(elapsedTime);
+
+    }, updateDelay)
+
+    isOn = true;
+};
+
+
+
+function handleClick() {
+    if (isOn === true) {
+        clearInterval(intervalID);
+        isOn = false;
+    } else {
+        startTimer();
+    }
+};
+
+timer.addEventListener("click", handleClick);
