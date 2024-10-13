26dd974ed1d8539dcb5070c2125882b85d846993
PJ <peterjoe2005@gmail.com>
2024-10-12 20:22:50 -0400

    added local script

--- public/src/game.js
+++ public/src/game.js
@@ -1,22 +1,21 @@
-import { Application, Assets, Sprite } from "../../node_modules/pixi.js/dist/pixi.mjs";
+import { Application, Assets, Sprite } from 'https://cdn.jsdelivr.net/npm/pixi.js@7.x/dist/pixi.mjs';
 
+(async () => {
+    const app = new Application({
+        backgroundColor: 0x1099bb, 
+        resizeTo: window, 
+    });
+    
+    document.body.appendChild(app.view); 
 
-(async () =>
-    {
-        // Create a new application
-        const app = new Application();
-    
-        // Initialize the application
-        await app.init({ background: '#1099bb', resizeTo: window });
-    
-        // Append the application canvas to the document body
-        document.body.appendChild(app.canvas);
-    
-        // Load the bunny texture
-        const texture = await Assets.load('https://ibb.co/M8pm8fm');
-        
+    const texture = await Assets.load('https://i.ibb.co/M8pm8fm/bunny.png'); 
 
+    const bunny = new Sprite(texture);
 
-    
-    })();
-    
+    bunny.x = app.screen.width / 2;
+    bunny.y = app.screen.height / 2;
+
+    bunny.anchor.set(0.5);
+
+    app.stage.addChild(bunny);
+})();
