087fd218bfb3ffdc41ce1aa927982af9a104e348
PJ <peterjoe2005@gmail.com>
2024-10-12 22:59:56 -0400

    added detroitpng

--- public/src/game.js
+++ public/src/game.js
@@ -14,10 +14,11 @@ import { getCarVelocity, getCarAngle } from './vision.mjs';
     document.body.appendChild(app.view);
 
     const car_texture = await Assets.load('/Assets/car.png');
-    const background_texture = await Assets.load('../Assets/placeholder_track.png');
+    const background_texture = await Assets.load('../Assets/detroitTopDownView.png');
 
     const car = new Sprite(car_texture);
     const background = new Sprite(background_texture);
+    
 
     car.x = app.screen.width / 2;
     car.y = app.screen.height / 2;
@@ -25,11 +26,15 @@ import { getCarVelocity, getCarAngle } from './vision.mjs';
     background.y = app.screen.height / 2;
     background.scale.set(8);
 
+    
+
     car.anchor.set(0.5);
     background.anchor.set(0.5);
 
+
     app.stage.addChild(background);
     app.stage.addChild(car);
+    
 
     app.ticker.add(() => {
         let targetAngle = getCarAngle();
