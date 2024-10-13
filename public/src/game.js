7c447d68a4680de101c0224caedf20e9ce89159e
KS055 <129549475+KS055@users.noreply.github.com>
2024-10-12 20:17:23 -0400

    added canvas to game.js

--- public/src/game.js
+++ public/src/game.js
@@ -1,7 +1,22 @@
 import { Application, Assets, Sprite } from "../../node_modules/pixi.js/dist/pixi.mjs";
 
-// Asynchronous IIFE
+
 (async () =>
     {
+        // Create a new application
+        const app = new Application();
+    
+        // Initialize the application
+        await app.init({ background: '#1099bb', resizeTo: window });
+    
+        // Append the application canvas to the document body
+        document.body.appendChild(app.canvas);
+    
+        // Load the bunny texture
+        const texture = await Assets.load('https://ibb.co/M8pm8fm');
         
+
+
+    
     })();
+    
