b1b22b22d045f4c4cafb10b3774a22019a7cbf50
Albert Chen <chenalbert@gmail.com>
2024-10-12 20:35:59 -0400

    WIP turning

--- public/src/game.js
+++ public/src/game.js
@@ -1,5 +1,6 @@
 import { Application, Assets, Sprite } from 'https://cdn.jsdelivr.net/npm/pixi.js@7.x/dist/pixi.mjs';
 
+import { getCarVelocity, getCarAngle } from './vision.mjs';
 (async () => {
     const app = new Application({
         backgroundColor: 0x1099bb, 
@@ -18,4 +19,20 @@ import { Application, Assets, Sprite } from 'https://cdn.jsdelivr.net/npm/pixi.j
     car.anchor.set(0.5);
 
     app.stage.addChild(car);
+    
+    app.ticker.add(() => {
+        let targetAngle = getCarAngle();
+
+        rotate(targetAngle);
+    });
+
+    
+    function rotate(targetAngle) {
+        if (car.angle < targetAngle) {
+            car.angle -= 1;
+        }
+        else if (car.angle > targetAngle) {
+            car.angle += 1;
+        }
+    }
 })();
